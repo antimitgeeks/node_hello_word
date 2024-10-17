a6f647f32aef68a5969f1506f613e14e218ac9f4
MikeITG <mike@itgeeks.com>
2024-10-17 12:40:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// Load HTTP module
+const http = require('http');
+require('dotenv').config();
+
+// Create an HTTP server
+http.createServer((req, res) => {
+    // Set the response HTTP header with HTTP status and Content type
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    // Send the response body "Hello World"
+    res.end('Hello World\n');
+}).listen(process.env.PORT, () => {
+    // Log message to console
+    console.log(`Server running at ${process.env.PORT}`);
+});
